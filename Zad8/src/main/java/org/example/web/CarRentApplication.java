080bf4e3147d5bb8555dadce4ab80ee7c0db612f
opilek <Jasopila@o2.pl>
2026-05-14 19:25:38 +0200

    Dodanie klasy startowej CarRentApplication

---
+++ Zad8/src/main/java/org/example/web/CarRentApplication.java
@@ -0,0 +1,16 @@
+package org.example.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CarRentApplication.class,args);
+
+    }
+
+
+}
