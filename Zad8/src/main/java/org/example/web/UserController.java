a0d974d34df979968626c3f3d29068b3cba3d35b
opilek <Jasopila@o2.pl>
2026-05-15 14:29:46 +0200

    Implementacja UserController

---
+++ Zad8/src/main/java/org/example/web/UserController.java
@@ -0,0 +1,35 @@
+package org.example.web;
+
+import org.example.models.User;
+import org.example.services.impl.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController
+{
+    private final UserService userService;
+
+    public UserController(UserService userService)
+    {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> list()
+    {
+        return userService.findAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable String id)
+    {
+        return userService.findById(id);
+
+    }
+}
