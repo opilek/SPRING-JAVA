5af4d703dd868ed3075188a6aa7688ca8794d852
opilek <Jasopila@o2.pl>
2026-05-15 16:57:58 +0200

    Dodanie adnotacji @EnableJpaRepositories, @EntityScan

--- Zad8/src/main/java/org/example/web/CarRentApplication.java
+++ Zad8/src/main/java/org/example/web/CarRentApplication.java
@@ -2,8 +2,13 @@ package org.example.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.example")
+@EnableJpaRepositories(basePackages = "org.example.repositories") // To zmusi skaner JPA do pracy
+@EntityScan(basePackages = "org.example.models")
 public class CarRentApplication
 {
     public static void main(String[] args)
